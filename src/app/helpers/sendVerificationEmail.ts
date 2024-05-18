eb87de9e8af5a05d0959990a09aa78a19f9cf39e
SMishra <smishra@kenmarkitan.com>
2024-05-18 17:11:50 +0530

    added resend and react-email sign-up logic

---
+++ src/app/helpers/sendVerificationEmail.ts
@@ -0,0 +1,22 @@
+import { resend } from "@/app/lib/resend";
+import VerificationEmail from "../../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+  await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: email,
+        subject: 'Mystery Message | Verification code',
+        react: VerificationEmail({username,otp:verifyCode}),
+    });
+    return { success: true, message: "Verification email send Successfully" };
+  } catch (emailError) {
+    console.error("error sending verification email", emailError);
+    return { success: false, message: "failed to send email" };
+  }
+}
