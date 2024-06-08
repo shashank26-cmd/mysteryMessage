c13f59c3a8b6e0ef4fab33c35f84eb2ef2f86f3c
SMishra <shashankbud80@gmail.com>
2024-06-08 23:45:31 +0530

    verification

--- src/app/helpers/sendVerificationEmail.ts
+++ src/app/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
   await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'Shashank@Mishra.dev',
         to: email,
         subject: 'Mystery Message | Verification code',
         react: VerificationEmail({username,otp:verifyCode}),
